bae2fd9c9e08294388dee161c3713f9c71e3190b
iatrz <76459973+iatrz@users.noreply.github.com>
2021-01-03 17:17:47 +0530

    revert /sys/class/graphics/fb*

--- multirom/mr_init_devices.c
+++ multirom/mr_init_devices.c
@@ -8,9 +8,9 @@
 const char *mr_init_devices[] =
 {
     // FrameBuffers
-    //"/sys/class/graphics/fb0",
+    "/sys/class/graphics/fb0",
     "/sys/devices/virtual/graphics/fb0",
-    //"/sys/class/graphics/fb1",
+    "/sys/class/graphics/fb1",
     "/sys/devices/virtual/graphics/fb1",
 
     // mmcblk
